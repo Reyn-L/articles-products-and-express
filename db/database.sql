b849f709977b39ac912df665e1b681782c4e6fe2
Reyn-L <Reyn.Leong@gmail.com>
2017-07-26 15:10:54 -1000

    created tables

--- db/database.sql
+++ db/database.sql
@@ -9,8 +9,21 @@ CREATE DATABASE products_db WITH OWNER products_user;
 \c products_db;
 
 CREATE TABLE products (
-id SERIAL NOT NULL PRIMARY KEY,
-name VARCHAR(100),
-price MONEY,
-inventory INT
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(100),
+  price MONEY,
+  inventory INT
+);
+
+CREATE TABLE authors (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+CREATE TABLE articles (
+  id SERIAL NOT NULL PRIMARY KEY,
+  title VARCHAR(100),
+  body VARCHAR(255),
+  url TEXT,
+  author_id INT REFERENCES authors(id)
 );
